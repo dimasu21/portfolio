95410e2db957dac10bbf6b73c5da1522ed959530
Dimas Tri M <dimsartz021@gmail.com>
2025-11-23 17:42:36 +0700

    update cv

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -203,12 +203,12 @@ const profile = {
 
                 {/* Contact Button */}
                 <a
-                  href="https://drive.google.com/file/d/1AOzfH27etuEG-bCVdc9ZqvJCc_mMoagn/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1raFJd1FAaKzRhNkkgj9dTQql7JLvy-Mp/view?usp=sharing"
                   className="group relative inline-flex items-center justify-center gap-3 p-0.5 rounded-xl bg-gradient-to-r from-gray-800 to-gray-700 transition-all duration-300 hover:scale-105 hover:shadow-[0_0_2rem_-0.5rem_#60A5FA]"
                 >
                   <span className="block w-full px-6 sm:px-8 py-3 sm:py-4 rounded-[11px] bg-gray-900 border border-gray-700/50 transition-all duration-300 group-hover:bg-gradient-to-r group-hover:from-gray-800 group-hover:to-gray-700">
                     <span className="relative flex items-center justify-center gap-2 text-gray-300 font-medium group-hover:text-white">
-                      <span>Get CV</span>
+                      <span>Hire Me</span>
                       <i className="fas fa-envelope transform transition-all duration-300 group-hover:rotate-12"></i>
                     </span>
                   </span>
